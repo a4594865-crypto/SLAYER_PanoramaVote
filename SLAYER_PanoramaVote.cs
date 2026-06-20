aef9f14610f5755eae84436bfb7eb30d6634d679
a4594865-crypto <a4594865@gmail.com>
2026-06-20 08:56:37 +0800

    Update SLAYER_PanoramaVote.cs

--- SLAYER_PanoramaVote.cs
+++ SLAYER_PanoramaVote.cs
@@ -144,7 +144,8 @@ public partial class SLAYER_PanoramaVote : BasePlugin
         if (args.Length < 2)
         {
             player.PrintToChat($" {Prefix} 使 用 方 法： {ChatColors.Yellow}.rtv de_mirage{ChatColors.White}");
-            player.PrintToChat($" {Prefix} 可 用 地 圖 池： {ChatColors.LightPurple}{string.Join(", ", _allowedMaps)}{ChatColors.White}");
+            player.PrintToChat($" {Prefix} 可 用 地 圖： {ChatColors.LightPurple}{string.Join(", ", _allowedMaps)}{ChatColors.White}");
+            player.PrintToChat($" {Prefix} 隨 機 分 隊： 請 改 輸 入 {ChatColors.Yellow}.vote shuffle{ChatColors.White}");
             return;
         }
 
